6122e44270b76e9948c9495c40242055bc4c9388
MaDongdong <donaldma@tencent.com>
2024-11-06 09:05:37 +0700

    format

--- modules/complete/outputs.tf
+++ modules/complete/outputs.tf
@@ -4,6 +4,6 @@ output "nat_gateway_id" {
 
 output "nat_route_ids" {
   value = {
-    for k, route in tencentcloud_route_table_entry.route_entry: k => route.route_item_id
+    for k, route in tencentcloud_route_table_entry.route_entry : k => route.route_item_id
   }
 }
